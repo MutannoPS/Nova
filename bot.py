c12926ff9ff1e8ea49604d67b9c54be60863a74b
MutannoPS <wemersonsergiosilva@gmail.com>
2025-07-22 00:36:21 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from flask import Flask, request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = os.environ.get("TOKEN", "7705145588:AAH3nZbWg8s_0HDA26xVYkjJ3o0oleYkDGA")
+TOKEN = os.environ.get("TOKEN", "7705145588:AAFzEufeVc45IpEiekpfxlkSe9gm_gS6dbA")
 PORT = int(os.environ.get("PORT", 8443))
 
 app = Flask(__name__)
